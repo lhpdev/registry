8630aa5645a576f6febfb25ec19c7b45866110bc
Lucas Hauptmann Pereira <lucass.hauptmann@gmail.com>
2024-12-05 13:00:40 -0300

    Update homepage and create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './pages/Root';
+import HomePage from './pages/home/HomePage';
+import { homeLoader } from './pages/home/homeLoader';
+import SearchPage from './pages/search/SearchPage';
+import { searchLoader } from './pages/search/searchLoader';
+import DetailsPage from './pages/details/DetailsPage';
+import { detailsLoader } from './pages/details/detailsLoader';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        loader: homeLoader
+      },
+      {
+       path: "/search",
+       element: <SearchPage />,
+       loader: searchLoader
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailsPage />,
+        loader: detailsLoader,
+      }
+    ]
+  }
+
+]);
 
 function App() {
-  return <div>Hi there</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
